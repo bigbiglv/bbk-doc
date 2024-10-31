41a04cb0f1778f450602f454c1fc5667b7b7fa3b
bbk <2845758942@qq.com>
2024-10-31 10:33:21 +0800

    移除live插件

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,6 @@ const config = {
       }),
     ],
   ],
-  themes: ['@docusaurus/theme-live-codeblock'],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
